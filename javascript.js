af31c6a1d3cc85ab2fd809a67e3226b475f3552e
FIRST_tristan LAST_richard <tristan3755@gmail.com>
2021-09-25 19:40:51 +0200

    responsive

--- javascript.js
+++ javascript.js
@@ -111,5 +111,93 @@ gauche.addEventListener('click', () => {
 
 })
 
+/*intersection observer*/
+let target = document.querySelector('.article1');
+let observer = new IntersectionObserver((entries) => {
+
+    entries.forEach((entry) => {
+      console.log(entry.isIntersecting)
+      console.log(entry.intersectionRatio)
+      if (entry.intersectionRatio>=0.5) {
+        console.log('j \'y suis')
+        target.style.opacity='1'
+        observer.disconnect()
+      }
+    })
+  },{threshold: 0.5});
 
 
+  observer.observe(target);
+
+
+  let target2 = document.querySelector('.article2');
+  let observer2 = new IntersectionObserver((entries) => {
+  
+      entries.forEach((entry) => {
+        console.log(entry.isIntersecting)
+        console.log(entry.intersectionRatio)
+        if (entry.intersectionRatio>=0.3) {
+          console.log('j \'y suis')
+          target2.style.opacity='1'
+          observer.disconnect()
+        }
+      })
+    },{threshold: 0.3});
+  
+  
+    observer2.observe(target2);
+
+    let target3 = document.querySelector('.article3');
+let observer3 = new IntersectionObserver((entries) => {
+
+    entries.forEach((entry) => {
+      console.log(entry.isIntersecting)
+      console.log(entry.intersectionRatio)
+      if (entry.intersectionRatio>=0.3) {
+        console.log('j \'y suis')
+        target3.style.opacity='1'
+        observer.disconnect()
+      }
+    })
+  },{threshold: 0.3});
+
+
+  observer3.observe(target3);
+
+
+  let target4 = document.getElementById('abonnement');
+  let observer4 = new IntersectionObserver((entries) => {
+  
+      entries.forEach((entry) => {
+        console.log(entry.isIntersecting)
+        console.log(entry.intersectionRatio)
+        if (entry.intersectionRatio>=0.2) {
+          console.log('j \'y suis')
+          target4.style.opacity='1'
+          target4.style.transform="translateX(0)"
+          observer.disconnect()
+        }
+      })
+    },{threshold: 0.2});
+  
+  
+    observer4.observe(target4);
+  
+
+    let target5 = document.getElementById('contactForm');
+    let observer5 = new IntersectionObserver((entries) => {
+    
+        entries.forEach((entry) => {
+          console.log(entry.isIntersecting)
+          console.log(entry.intersectionRatio)
+          if (entry.intersectionRatio>=0.2) {
+            console.log('j \'y suis')
+            target5.style.opacity='1'
+            target5.style.transform="translateY(0)"
+            observer.disconnect()
+          }
+        })
+      },{threshold: 0.2});
+    
+    
+      observer5.observe(target5);
